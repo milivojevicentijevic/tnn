4b59c5810391b9c50d6b998af8f528658b3d5ab7
milivojevicentijevic <milivojevicentijevic985@gmail.com>
2021-02-02 13:44:59 +0100

    validation style

--- OOPHP/index.php
+++ OOPHP/index.php
@@ -1,3 +1,26 @@
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" type="text/css" href="styles.css">
+    <title>Document</title>
+</head>
+<body>
 <?php
-    echo "Hello, ninjas!"
+    if(isset($_POST['submit'])) {
+        // validate entries
+        echo "form submitted";
+    }
 ?>
+    <div class="new-user">
+        <h2>Create new user</h2>
+        <form action="<?php echo $_SERVER['PHP_SELF']?>" method="POST">
+            <label>Username:</label>
+            <input type="text" name="username">
+            <label>Email:</label>
+            <input type="text" name="email">
+            <input type="submit" value="submit" name="submit">
+        </form>
+    </div>
+</body>
+</html>
