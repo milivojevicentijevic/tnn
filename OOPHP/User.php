2ea0cd7948ecf888c5e7c67905bf59f1e080b96f
milivojevicentijevic <milivojevicentijevic985@gmail.com>
2021-02-02 11:04:38 +0100

    constructors

--- OOPHP/User.php
+++ OOPHP/User.php
@@ -1,11 +1,14 @@
 <?php
 class User 
 {
-    public $username = 'jovan';
-    public $email = 'jovan@yahoo.com';
-
+    public $username;
+    public $email;
+    
+    public function __construct($username, $email) {
+        $this->username = $username;
+        $this->email = $email;
+    }
     public function addFriend() {
-        
         return "$this->username added a new friend";
     }
 }
--- OOPHP/intro.php
+++ OOPHP/intro.php
@@ -7,22 +7,16 @@
 <body>
     <?php
         require 'User.php';
-        $user1 = new User();
-        $user2 = new User();
+        $user1 = new User('jovan', 'jovan@yahoo.com');
+        $user2 = new User('mitar', 'mitar@yahoo.com');
 
         echo $user1->username."<br>";
         echo $user1->email."<br>";
         echo $user1->addFriend()."<br>";
 
-        $user2->username = 'mitar';
-        $user2->email = 'mitar@yahoo.com';
         echo $user2->username."<br>";
         echo $user2->email."<br>";
         echo $user2->addFriend()."<br>";
-
-        print_r(get_class_vars('User'));
-        echo "<br>";
-        print_r(get_class_methods('User'));
     ?>
 </body>
 </html>
@@ -39,6 +33,4 @@ jovan added a new friend
 mitar
 mitar@yahoo.com
 mitar added a new friend
-Array ( [username] => jovan [email] => jovan@yahoo.com )
-Array ( [0] => addFriend ) 
 -->
